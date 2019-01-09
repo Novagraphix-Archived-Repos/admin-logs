d660a3330465e84f5c2f3fcb6abeb4dc1d232ceb
Stephan Jess <s.jess@novagraphix.de>
2019-01-09 10:09:50 +0100

    first commit

---
+++ src/Tool.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Novagraphix\NovaLogViewer;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool as BaseTool;
+
+class Tool extends BaseTool
+{
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::script('nova-log-viewer', __DIR__.'/../dist/js/tool.js');
+        // Not needed until css is written.
+        // Nova::style('nova-log-viewer', __DIR__.'/../dist/css/tool.css');
+    }
+
+    /**
+     * Build the view that renders the navigation links for the tool.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function renderNavigation()
+    {
+        return view('nova-log-viewer::navigation');
+    }
+}
---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Novagraphix\NovaLogViewer;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Novagraphix\NovaLogViewer\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'nova-log-viewer');
+
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            Nova::provideToScript([
+                'viewer' => config('log-viewer.colors.levels'),
+            ]);
+        });
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Route::middleware(['nova', Authorize::class])
+                ->prefix('nova-vendor/novagraphix/nova-log-viewer')
+                ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
